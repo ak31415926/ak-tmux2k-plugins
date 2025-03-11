2cbca284bba3d00b28c6906401aec686ff06f673
ak31415926 <kuchumov@ak.pp.ru>
2025-03-12 00:25:04 +0300

    Add install script and setting default colors

--- ak-tmux2k-plugins.tmux
+++ ak-tmux2k-plugins.tmux
@@ -2,3 +2,15 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 tmux bind-key S run-shell "$CURRENT_DIR/scripts/install.sh"
+
+declare -A plugin_colors=(
+    ["a_uptime"]="light_blue text"
+)
+
+main() {
+	for plugin in "${!plugin_colors[@]}"; do
+		tmux set-option -g @tmux2k-${plugin}-colors "${plugin_colors[$plugin]}"
+    done
+}
+
+main
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,40 @@
 
 # fetching the directory where plugins are installed
 plugin_path="$(tmux show-env -g TMUX_PLUGIN_MANAGER_PATH | cut -f2 -d=)"
+tmux2k_path="${plugin_path}tmux2k"
+ak_plugins_path="${plugin_path}ak-tmux2k-plugins"
+
+exit() {
+	echo "Press ESC to continue"
+	builtin exit $1
+}
 
 # listing installed plugins
-ls -1 "$plugin_path"
+if [ -d "$tmux2k_path" ]; then 
+	echo "tmux2k found in $tmux2k_path"
+else
+	echo "tmux2k NOT found in $tmux2k_path"
+	exit
+fi
+
+if [ -d "$ak_plugins_path" ]; then 
+	echo "ak-tmux2k-plugins found in $ak_plugins_path"
+else
+	echo "ak-tmux2k-plugins NOT found in $ak_plugins_path"
+	exit
+fi
+
+echo "Making symbolic links"
+
+for f in `ls ${ak_plugins_path}/plugins`; do
+#	echo "plugins/$f"
+	ln -is ${ak_plugins_path}/plugins/${f} ${tmux2k_path}/plugins/${f}
+done
+
+for f in `ls ${ak_plugins_path}/lib`; do
+	ln -is ${ak_plugins_path}/lib/${f} ${tmux2k_path}/lib/${f}
+done
+
+echo "Done"
+
+exit 0
