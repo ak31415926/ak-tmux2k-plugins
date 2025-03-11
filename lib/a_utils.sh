6b15f88553872ba21014ea6294b4e851c64944aa
ak31415926 <kuchumov@ak.pp.ru>
2025-03-12 02:46:54 +0300

    Modifying format string for uptime plugin

--- lib/a_utils.sh
+++ lib/a_utils.sh
@@ -1,2 +1,46 @@
 #!/usr/bin/env bash
 
+get_tmux_option() {
+    local option=$1
+    local default_value=$2
+    local option_value=$(tmux show-option -gqv "$option")
+    if [ -z "$option_value" ]; then
+        echo $default_value
+    else
+        echo $option_value
+    fi
+}
+
+normalize_padding() {
+    percent_len=${#1}
+    max_len=${2:-4}
+    let diff_len=$max_len-$percent_len
+    # if the diff_len is even, left will have 1 more space than right
+    let left_spaces=($diff_len + 1)/2
+    let right_spaces=($diff_len)/2
+    printf "%${left_spaces}s%s%${right_spaces}s\n" "" $1 ""
+}
+
+fill_placeholders() {
+# Usage: fill_placeholders formatstring placeholder type value
+	result=$1
+	while [[ "$result" =~ \#(.?[[:digit:]]*\.?[[:digit:]]+)(${2}) ]];
+	do
+		tfs="%${BASH_REMATCH[1]}$3"
+		printf -v val ${tfs} $4
+		result=${result//${BASH_REMATCH[0]}/$val}
+	done
+	result=${result//\#${2}/${4}}
+	echo -n "$result"
+}
+
+normalize_brackets() {
+# Usage: fill_placeholders formatstring
+	result=$1
+	while [[ "$result" =~ \#([1-9][0-9]*)\[([^\]]*)\#\] ]];
+	do
+		content="$(normalize_padding ${BASH_REMATCH[2]} ${BASH_REMATCH[1]})"
+		result=${result/"${BASH_REMATCH[0]}"/$content}
+	done
+	echo -n "$result"
+}
--- plugins/a_uptime.sh
+++ plugins/a_uptime.sh
@@ -1,34 +1,9 @@
 #!/usr/bin/env bash
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$current_dir/../lib/utils.sh"
+source "$current_dir/../lib/a_utils.sh"
 
 export LC_ALL="en_US.UTF-8"
-
-
-fill_placeholders() {
-# Usage: fill_placeholders formatstring placeholder type value
-	result=$1
-	while [[ "$result" =~ %([^%#]*)(${2}) ]];
-	do
-		tfs="%${BASH_REMATCH[1]}$3"
-		printf -v val ${tfs} $4
-		result=${result//${BASH_REMATCH[0]}/$val}
-	done
-	result=${result//$2/$4}
-	echo -n "$result"
-}
-
-normalize_brackets() {
-# Usage: fill_placeholders formatstring
-	result=$1
-	while [[ "$result" =~ %([1-9][0-9]*)\[([^\]]*)%\] ]];
-	do
-		content="$(normalize_padding ${BASH_REMATCH[2]} ${BASH_REMATCH[1]})"
-		result=${result/"${BASH_REMATCH[0]}"/$content}
-	done
-	echo -n "$result"
-}
 
 get_uptime() {
     uptime -p | awk '
@@ -48,7 +23,7 @@ get_uptime() {
 }
 
 main() {
-	fs=$(get_tmux_option "@tmux2k-uptime2-format-string" "󰀠 %D[#dd %02#hh %02#mm%D]%d[%H[#hh %02#mm%H]%h[%2#m min%h]%d]")
+	fs=$(get_tmux_option "@tmux2k-a_uptime-format-string" "󰀠 #D[#dd #02hh #02mm#D]#d[#H[#hh #02mm#H]#h[#2m min#h]#d]")
 	user_locale=$(get_tmux_option "@tmux2k-user-locale" "en_US.UTF-8")
 	export LC_ALL="$user_locale"
 
@@ -58,33 +33,33 @@ main() {
 	IFS=$t_ifs
 
 	if [ "$days" = "" ]; then
-		fs=${fs//%D\[*%D\]/}
-		fs=${fs//%d\[/}
-		fs=${fs//%d\]/}
+		fs=${fs//\#D\[*\#D\]/}
+		fs=${fs//\#d\[/}
+		fs=${fs//\#d\]/}
 		days=0
 	else
-		fs=${fs//%d\[*%d\]/}
-		fs=${fs//%D\[/}
-		fs=${fs//%D\]/}
+		fs=${fs//\#d\[*\#d\]/}
+		fs=${fs//\#D\[/}
+		fs=${fs//\#D\]/}
 	fi
 	if [ "$hours" = "" ]; then
-		fs=${fs//%H\[*%H\]/} 
-		fs=${fs//%h\[/}
-		fs=${fs//%h\]/}
+		fs=${fs//\#H\[*\#H\]/} 
+		fs=${fs//\#h\[/}
+		fs=${fs//\#h\]/}
 		hours=0
 	else
-		fs=${fs//%h\[*%h\]/}
-		fs=${fs//%H\[/}
-		fs=${fs//%H\]/}
+		fs=${fs//\#h\[*\#h\]/}
+		fs=${fs//\#H\[/}
+		fs=${fs//\#H\]/}
 	fi
-	fs=$(fill_placeholders "$fs" "#d" "d" $days)
-	fs=$(fill_placeholders "$fs" "#h" "d" $hours)
-	fs=$(fill_placeholders "$fs" "#m" "d" $minutes)
-	fs=$(fill_placeholders "$fs" "#H" "d" $fullhours)      # full hours (days*24+hours)
-	fs=$(fill_placeholders "$fs" "#M" "d" $fullminutes)    # full minutes ((days*24+hours)*60+minutes)
-	fs=$(fill_placeholders "$fs" "#D" "f" $fracdays)       # fractional days  days+((hours*60+minutes)/(24*60))
-	fs=$(fill_placeholders "$fs" "#F" "f" $fracfullhours)  # full fractional hours (full hours)+(minutes/60)
-	fs=$(fill_placeholders "$fs" "#f" "f" $frachours)      # fractional hours (hours)+(minutes/60)
+	fs=$(fill_placeholders "$fs" "d" "d" $days)
+	fs=$(fill_placeholders "$fs" "h" "d" $hours)
+	fs=$(fill_placeholders "$fs" "m" "d" $minutes)
+	fs=$(fill_placeholders "$fs" "H" "d" $fullhours)      # full hours (days*24+hours)
+	fs=$(fill_placeholders "$fs" "M" "d" $fullminutes)    # full minutes ((days*24+hours)*60+minutes)
+	fs=$(fill_placeholders "$fs" "D" "f" $fracdays)       # fractional days  days+((hours*60+minutes)/(24*60))
+	fs=$(fill_placeholders "$fs" "F" "f" $fracfullhours)  # full fractional hours (full hours)+(minutes/60)
+	fs=$(fill_placeholders "$fs" "f" "f" $frachours)      # fractional hours (hours)+(minutes/60)
 
 	fs=$(normalize_brackets "$fs")
 
